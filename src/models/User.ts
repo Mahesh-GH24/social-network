a6d5dc1aa36a266ae83989ab776d02d943da40c7
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-01-17 19:42:48 -0600

    Added User model code

---
+++ src/models/User.ts
@@ -0,0 +1,58 @@
+import { Schema, Document } from 'mongoose';
+
+//interface for User
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: Schema.Types.ObjectId[];
+    friends: Schema.Types.ObjectId[];
+}
+
+//Schema to create User model
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            unique:true,
+            required: true, 
+            trim: true,
+        },
+        email: {
+            type: String,
+            required:true,
+            unique:true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Email validation failed'
+            }
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+            },
+        ]
+    },
+    {
+        //Include Virtuals
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+//Create Virtual called friendCount
+userSchema
+    .virtual('friendCount')
+    //Getter
+    .get(function (this: any) {
+        return `${this.friends.length}`;
+    }
+)

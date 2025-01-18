1ec71aa24989aba4f2da43babbb05210433d1668
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-01-17 21:02:38 -0600

    Added UserController methods for get and create

---
+++ src/controllers/userController.ts
@@ -0,0 +1,32 @@
+import User from '../models/User.js';
+import { Request, Response } from 'express';
+
+//create a new user
+export const createUser = async (req: Request, res: Response) => {
+    try{
+        const dbUserData = await User.create(req.body);
+        res.json(dbUserData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+//get all users
+export const getUsers = async(_req: Request, res:Response) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+//get user by id
+export const getUserById = async(req: Request, res:Response) => {
+    try{
+        const users = await User.findOne({_id:req.params.userId})
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
